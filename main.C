6946be2c6b01234e0f37aa20e99a2b5566a47691
karim-lazghab <87267054+karim-lazghab@users.noreply.github.com>
2023-12-23 22:07:39 +0100

    Update main.C

--- main.C
+++ main.C
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-// Structure pour stocker les informations des candidats
+
 typedef struct {
     char nom[100];
     char prenom[100];
@@ -8,7 +8,6 @@ typedef struct {
     int nombre_de_votes;
 } Candidat;
 
-// Structure pour stocker les informations des élections
 typedef struct {
     char nom[100];
     Candidat candidats[100];
@@ -16,9 +15,8 @@ typedef struct {
 } Election;
 
 typedef struct {
-    char nom_utilisateur[50];      // Nom de l'administrateur
-    char mot_de_passe[50];   // mdp de l'administrateur
-    // Ajoutez d'autres champs si nécessaire
+    char nom_utilisateur[50];
+    char mot_de_passe[50];
 } administrateur;
 
 administrateur admins[100];
@@ -26,12 +24,9 @@ Candidat candidats[100];
 Election elections[100];
 int nombre_d_elections = 0;
 
-//stocker les informations dans les tableaux
 void load() {
     FILE *file;
     int i;
-
-    // Charger les administrateurs
     file = fopen("admin.txt", "r");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier admin\n");
@@ -43,7 +38,6 @@ void load() {
     }
     fclose(file);
 
-    // Charger les candidats
     file = fopen("candidat.txt", "r");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier candidat\n");
@@ -55,89 +49,83 @@ void load() {
     }
     fclose(file);
 
-    // Charger les élections
     file = fopen("election.txt", "r");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier election\n");
         return;
     }
     i = 0;
+    fread(&nombre_d_elections, sizeof(int), 1, file);
     while (fread(&elections[i], sizeof(Election), 1, file)) {
         i++;
     }
     fclose(file);
 }
 
-// Fonctions pour gérer les élections
+
 void creer_compte_administrateur() {
     administrateur new_admin;
     printf("Entrez le nom de l'administrateur: ");
     scanf("%s", new_admin.nom_utilisateur);
     printf("Entrez le mot de passe de l'administrateur: ");
     scanf("%s", new_admin.mot_de_passe);
-    // Add the new admin to the admins array
     admins[nombre_d_elections] = new_admin;
     nombre_d_elections++;
 }
 
-
 void creer_compte_candidat() {
     Candidat new_candidat;
     printf("Entrez le nom du candidat: ");
-    if(scanf("%s", new_candidat.nom) != 1) {
+    if (scanf("%s", new_candidat.nom) != 1) {
         printf("Erreur lors de la saisie du nom.\n");
         return;
     }
     printf("Entrez le prenom du candidat: ");
-    if(scanf("%s", new_candidat.prenom) != 1) {
+    if (scanf("%s", new_candidat.prenom) != 1) {
         printf("Erreur lors de la saisie du prenom.\n");
         return;
     }
     printf("Entrez l'age du candidat: ");
-    if(scanf("%d", &new_candidat.age) != 1 || new_candidat.age < 0) {
+    if (scanf("%d", &new_candidat.age) != 1 || new_candidat.age < 0) {
         printf("Erreur lors de la saisie de l'age.\n");
         return;
     }
-    new_candidat.nombre_de_votes = 0;  // Initialize the number of votes to 0
-
-    // Add the new candidate to the candidates array
+    new_candidat.nombre_de_votes = 0;
     candidats[nombre_d_elections] = new_candidat;
     nombre_d_elections++;
 }
 
-
 void creer_nouvelle_election() {
     Election new_election;
     printf("Entrez le nom de l'election: ");
-    if(scanf("%s", new_election.nom) != 1) {
+    if (scanf("%s", new_election.nom) != 1) {
         printf("Erreur lors de la saisie du nom.\n");
         return;
     }
     printf("Entrez le nombre de candidats: ");
-    if(scanf("%d", &new_election.nombre_de_candidats) != 1 || new_election.nombre_de_candidats < 0) {
+    if (scanf("%d", &new_election.nombre_de_candidats) != 1 || new_election.nombre_de_candidats < 0) {
         printf("Erreur lors de la saisie du nombre de candidats.\n");
         return;
     }
-    for(int i = 0; i < new_election.nombre_de_candidats; i++) {
-        printf("Entrez le nom du candidat %d: ", i+1);
-        if(scanf("%s", new_election.candidats[i].nom) != 1) {
+    for (int i = 0; i < new_election.nombre_de_candidats; i++) {
+        printf("Entrez le nom du candidat %d: ", i + 1);
+        if (scanf("%s", new_election.candidats[i].nom) != 1) {
             printf("Erreur lors de la saisie du nom du candidat.\n");
             return;
         }
-        printf("Entrez le prenom du candidat %d: ", i+1);
-        if(scanf("%s", new_election.candidats[i].prenom) != 1) {
+        printf("Entrez le prenom du candidat %d: ", i + 1);
+        if (scanf("%s", new_election.candidats[i].prenom) != 1) {
             printf("Erreur lors de la saisie du prenom du candidat.\n");
             return;
         }
-        printf("Entrez l'âge du candidat %d: ", i+1);
-        if(scanf("%d", &new_election.candidats[i].age) != 1 || new_election.candidats[i].age < 0) {
+        printf("Entrez l'âge du candidat %d: ", i + 1);
+        if (scanf("%d", &new_election.candidats[i].age) != 1 || new_election.candidats[i].age < 0) {
             printf("Erreur lors de la saisie de l'âge du candidat.\n");
             return;
         }
-        new_election.candidats[i].nombre_de_votes = 0;  // Initialize the number of votes to 0
+        new_election.candidats[i].nombre_de_votes = 0;
     }
 
-    // Add the new election to the elections array
     elections[nombre_d_elections] = new_election;
     nombre_d_elections++;
 }
@@ -148,26 +136,25 @@ void mettre_a_jour_info_candidat() {
     int found = 0;
 
     printf("Entrez le nom du candidat: ");
-    if(scanf("%s", nom) != 1) {
+    if (scanf("%s", nom) != 1) {
         printf("Erreur lors de la saisie du nom.\n");
         return;
     }
     printf("Entrez le prenom du candidat: ");
-    if(scanf("%s", prenom) != 1) {
+    if (scanf("%s", prenom) != 1) {
         printf("Erreur lors de la saisie du prenom.\n");
         return;
     }
 
-    // Search for the candidate in the candidates array
-    for(int i = 0; i < nombre_d_elections; i++) {
-        if(strcmp(candidats[i].nom, nom) == 0 && strcmp(candidats[i].prenom, prenom) == 0) {
+    for (int i = 0; i < nombre_d_elections; i++) {
+        if (strcmp(candidats[i].nom, nom) == 0 && strcmp(candidats[i].prenom, prenom) == 0) {
             printf("Entrez le nouvel âge du candidat: ");
-            if(scanf("%d", &candidats[i].age) != 1 || candidats[i].age < 0) {
+            if (scanf("%d", &candidats[i].age) != 1 || candidats[i].age < 0) {
                 printf("Erreur lors de la saisie de l'âge.\n");
                 return;
             }
             printf("Entrez le nouveau nombre de votes du candidat: ");
-            if(scanf("%d", &candidats[i].nombre_de_votes) != 1 || candidats[i].nombre_de_votes < 0) {
+            if (scanf("%d", &candidats[i].nombre_de_votes) != 1 || candidats[i].nombre_de_votes < 0) {
                 printf("Erreur lors de la saisie du nombre de votes.\n");
                 return;
             }
@@ -176,23 +163,23 @@ void mettre_a_jour_info_candidat() {
         }
     }
 
-    if(!found) {
+    if (!found) {
         printf("Candidat non trouve.\n");
     }
 }
 
 void afficher_elections() {
-    if(nombre_d_elections == 0) {
+    if (nombre_d_elections == 0) {
         printf("Aucune election n'a ete creee.\n");
         return;
     }
 
-    for(int i = 0; i < nombre_d_elections; i++) {
-        printf("Election %d:\n", i+1);
+    for (int i = 0; i < nombre_d_elections; i++) {
+        printf("Election %d:\n", i + 1);
         printf("Nom de l'election: %s\n", elections[i].nom);
         printf("Nombre de candidats: %d\n", elections[i].nombre_de_candidats);
-        for(int j = 0; j < elections[i].nombre_de_candidats; j++) {
-            printf("Candidat %d:\n", j+1);
+        for (int j = 0; j < elections[i].nombre_de_candidats; j++) {
+            printf("Candidat %d:\n", j + 1);
             printf("Nom: %s\n", elections[i].candidats[j].nom);
             printf("Prenom: %s\n", elections[i].candidats[j].prenom);
             printf("Âge: %d\n", elections[i].candidats[j].age);
@@ -201,18 +188,19 @@ void afficher_elections() {
         printf("\n");
     }
 }
+
 void afficher_liste_votes() {
-    if(nombre_d_elections == 0) {
+    if (nombre_d_elections == 0) {
         printf("Aucune election n'a ete creee.\n");
         return;
     }
 
-    for(int i = 0; i < nombre_d_elections; i++) {
-        printf("Election %d:\n", i+1);
+    for (int i = 0; i < nombre_d_elections; i++) {
+        printf("Election %d:\n", i + 1);
         printf("Nom de l'election: %s\n", elections[i].nom);
         printf("Nombre de candidats: %d\n", elections[i].nombre_de_candidats);
-        for(int j = 0; j < elections[i].nombre_de_candidats; j++) {
-            printf("Candidat %d:\n", j+1);
+        for (int j = 0; j < elections[i].nombre_de_candidats; j++) {
+            printf("Candidat %d:\n", j + 1);
             printf("Nom: %s\n", elections[i].candidats[j].nom);
             printf("Prenom: %s\n", elections[i].candidats[j].prenom);
             printf("Nombre de votes: %d\n", elections[i].candidats[j].nombre_de_votes);
@@ -221,10 +209,40 @@ void afficher_liste_votes() {
     }
 }
 
+void rechercher_candidat() {
+    if (nombre_d_elections == 0) {
+        printf("Aucune election n'a ete creee.\n");
+        return;
+    }
+
+    char nom_recherche[100];
+    printf("Entrez le nom du candidat que vous souhaitez rechercher : ");
+    scanf("%s", nom_recherche);
+
+    int found = 0;
+
+    printf("Candidats trouves avec le nom '%s' :\n", nom_recherche);
+    for (int i = 0; i < nombre_d_elections; i++) {
+        for (int j = 0; j < elections[i].nombre_de_candidats; j++) {
+            if (strcmp(elections[i].candidats[j].nom, nom_recherche) == 0) {
+                printf("Election : %s\n", elections[i].nom);
+                printf("Nom : %s\n", elections[i].candidats[j].nom);
+                printf("Prenom : %s\n", elections[i].candidats[j].prenom);
+                printf("Age : %d\n", elections[i].candidats[j].age);
+                printf("Nombre de votes : %d\n", elections[i].candidats[j].nombre_de_votes);
+                found = 1;
+            }
+        }
+    }
+
+    if (!found) {
+        printf("Aucun candidat trouve avec le nom '%s'.\n", nom_recherche);
+    }
+}
+
 void save() {
     FILE *file;
 
-    // Sauvegarder les administrateurs
     file = fopen("admin.txt", "w");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier admin pour sauvegarde.\n");
@@ -233,7 +251,6 @@ void save() {
     fwrite(admins, sizeof(administrateur), nombre_d_elections, file);
     fclose(file);
 
-    // Sauvegarder les candidats
     file = fopen("candidat.txt", "w");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier candidat pour sauvegarde.\n");
@@ -242,12 +259,12 @@ void save() {
     fwrite(candidats, sizeof(Candidat), nombre_d_elections, file);
     fclose(file);
 
-    // Sauvegarder les élections
     file = fopen("election.txt", "w");
     if (file == NULL) {
         printf("Impossible d'ouvrir le fichier election pour sauvegarde.\n");
         return;
     }
+    fwrite(&nombre_d_elections, sizeof(int), 1, file);
     fwrite(elections, sizeof(Election), nombre_d_elections, file);
     fclose(file);
 
@@ -256,8 +273,9 @@ void save() {
 
 int main() {
     int choix;
-    load(); //stock les informations dans des tableaux respective
-    do{    
+    load();
+
+    do {
         printf("**********************************************************************\n");
         printf("Veuillez selectionner une fonction :\n");
         printf("1. Creer un compte administrateur\n");
@@ -266,11 +284,11 @@ int main() {
         printf("4. Mettre a jour les informations du candidat\n");
         printf("5. Afficher les elections\n");
         printf("6. Afficher la liste des votes\n");
-        printf("7. Quitter\n");
+        printf("7. rechercher un candidat\n");
+        printf("8. Quitter\n");
         printf("**********************************************************************\n");
         scanf("%d", &choix);
-
-        switch(choix) {
+        switch (choix) {
             case 1:
                 creer_compte_administrateur();
                 break;
@@ -290,13 +308,18 @@ int main() {
                 afficher_liste_votes();
                 break;
             case 7:
-                save();// sauvegarde les informations dans des fichiers 
+                rechercher_candidat();
+                break;
+            case 8:
+                save();
                 break;
             default:
                 printf("Choix non valide\n");
+                printf("quitter!");
+                return 0;
         }
-    }while(choix!=7);
+    } while (choix != 8);
+
     return 0;
 }
-
-//NOT UPDATED
+//not updated
